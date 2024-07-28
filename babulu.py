6e7b22e409464a4f4b41b74b5b5bcdc4a7198cbb
Ajgit8886 <Ajgit8886>
2024-07-28 06:22:49 +0000

    commit

---
+++ babulu.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE TABLE employees1  (
+# MAGIC     id INT ,
+# MAGIC     first_name VARCHAR(50),
+# MAGIC     last_name VARCHAR(50),
+# MAGIC     position VARCHAR(50),
+# MAGIC     salary DECIMAL(10, 2),
+# MAGIC     hire_date DATE
+# MAGIC );
+# MAGIC
